485ad26e5e31b3add224e612736b5431a8a5fb11
Aman Tripathi <amanttripathi02@gmail.com>
2026-01-13 12:33:36 +0530

    created routes for all the post messages

---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const postController = require("../controllers/postController");
+const verifyToken = require("../middleware/auth");
+
+
+router.post("/", verifyToken, postController.createPost);
+router.get("/:id", verifyToken, postController.getPost);
+router.put("/:id", verifyToken, postController.updatePost);
+router.delete("/:id", verifyToken, postController.deletePost);
+router.put("/:id/like", verifyToken, postController.likePost);
+router.get("/:id/timeline/all", verifyToken, postController.getTimelinePosts);
+
+module.exports = router;
